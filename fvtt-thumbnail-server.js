60b035ff5f43a6750cdb7baf25bc7f9717430dc5
LucasVon <von@wotlabs.net>
2020-08-11 09:48:04 +0200

    Update fvtt-thumbnail-server.js

--- fvtt-thumbnail-server.js
+++ fvtt-thumbnail-server.js
@@ -1,15 +1,3 @@
-Hooks.on("init", () => {
-  const oldFilePickerOptions = FilePicker.defaultOptions;
-  Object.defineProperty(FilePicker, "defaultOptions", {
-    get: () => {
-      return mergeObject(oldFilePickerOptions, {
-        template: "modules/fvtt-thumbnail-server/filepicker.html",
-        classes: ["filepicker"],
-        width: 500,
-        tabs: [{navSelector: ".tabs"}],
-        resizable: true
-      });
-    }
-  });
-  console.log(FilePicker.defaultOptions.template);
+Hooks.on("renderFilePicker", () => {
+  console.log(FilePicker.results);
 });
