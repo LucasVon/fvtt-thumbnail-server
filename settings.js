dfc0edb040e3477ef2c69a176022b588be161a32
LucasVon <von@wotlabs.net>
2020-08-11 12:30:02 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,7 @@ export const registerSettings = function() {
         hint: "Enter the URL of the thumbnail reverse proxy server. The expected usage with this module is to set up nginx to generate and serve thumbnails simply using a reverse proxy server.<br />Example: https://i.example.com/",
         scope: "world",
         config: true,
+        default: "",
         type: String
       });
 }
