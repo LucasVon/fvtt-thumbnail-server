7ffec77732cce74fdd6b49163b239b6ad9ae164d
LucasVon <von@wotlabs.net>
2020-08-11 10:31:41 +0200

    Update fvtt-thumbnail-server.js

--- fvtt-thumbnail-server.js
+++ fvtt-thumbnail-server.js
@@ -1,4 +1,4 @@
-Hooks.on('renderFilePicker', (data) => {
+Hooks.on('renderFilePicker', (app, html, data) => {
   console.log(data);
   data.files.forEach(function(image) {
     image.img = "https://play.erewar.com/" + image.img;
