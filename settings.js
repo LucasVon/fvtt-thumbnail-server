2aa496252d109ddb9c9e135f2614eb569a7bb1d8
LucasVon <von@wotlabs.net>
2020-08-11 12:26:00 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,9 @@
 export const registerSettings = function() {
     game.settings.register("fvtt-thumbnail-server", "thumbnailServer", {
         name: "Thumbnail Server",
-        hint: "Enter the URL of the thumbnail server. Foundry will expect the thumbnail to be located in the exact same path as the actual image. Example: https://i.example.com/",
+        hint: "Enter the URL of the thumbnail reverse proxy server. The expected usage with this module is to set up nginx to generate and serve thumbnails simply using a reverse proxy server.<br />Example: https://i.example.com/",
         scope: "world",
         config: true,
-        type: String,
-        default: false
+        type: String
       });
 }
